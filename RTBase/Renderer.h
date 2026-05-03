2196d4057af5510f7e4be54e6ffd819964578c52
Nooobad <isalexpro1615@qq.com>
2026-05-03 22:34:33 +0100

    Update

--- RTBase/Renderer.h
+++ RTBase/Renderer.h
@@ -840,12 +840,12 @@ public:
 	}
 	void saveFinalOutputs(std::string& baseName) {
 		std::vector<Colour> noisyPixels = currentImagePixels();
-		//saveHDRFromPixels(baseName + ".hdr", noisyPixels);
+		saveHDRFromPixels(baseName + ".hdr", noisyPixels);
 		savePNGFromPixels(baseName + ".png", noisyPixels);
 
 		std::vector<Colour> denoisedPixels;
 		if (denoise(denoisedPixels)) {
-			//saveHDRFromPixels(baseName + "-denoised.hdr", denoisedPixels);
+			saveHDRFromPixels(baseName + "-denoised.hdr", denoisedPixels);
 			savePNGFromPixels(baseName + "-denoised.png", denoisedPixels);
 		}
 	}
